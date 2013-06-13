0cc74a994de43a3ec53e70191cfa292de543c3c2
Albert Fabregat <afabregat@bmat.com>
2013-06-13 18:24:21 +0200

    Added musixmatch fallback

--- lib/mhd/run.py
+++ lib/mhd/run.py
@@ -1,4 +1,4 @@
-import os, web, socket, json, pprint, time
+import os, web, json
 import pyella
 import re
 
@@ -15,16 +15,40 @@ urls = (
     )
 app = web.application(urls, globals())
 
+apikey = "905e7eec68193e3f0a81a75591b4a518"
 
-def get_musixmatch_lyric(url, apikey):
+def get_mbi_from_url(url):
+    mbi = url.replace("http://musicbrainz.org/track/", "")
+    mbi = mbi.replace(".html", "")
+    return mbi
+
+def request_to_json(url):
     import urllib2
     req = urllib2.Request(url, None, {'Content-Type': 'application/json'})
     f = urllib2.urlopen(req)
     response = f.read()
     json_dict = json.loads(response)
-    print json_dict
     f.close()
-    return json_dict['message']['body']['lyrics']['lyrics_body']
+    return json_dict
+
+
+def get_musixmatch_lyric(mbid, type_track="track_mbid"):
+    url = ("http://api.musixmatch.com/ws/1.1/track.subtitle.get"
+            "?%s=%s&"
+           "apikey=%s&format=json" % (type_track, mbid, apikey))
+    json = request_to_json(url)
+    print json
+    return json['message']['body']['subtitle']['subtitle_body']
+
+
+def get_musixmatch_lyric_by_query(query):
+    url = ("http://api.musixmatch.com/ws/1.1/"
+            "track.search?q=%s&f_has_lyrics=1&format=json&apikey=%s"
+           % (query, apikey))
+    json = request_to_json(url)
+    track_id = json['message']['body']['track_list'][0]['track']['track_id']
+    print track_id
+    return get_musixmatch_lyric(track_id, "track_id")
 
 def return_composite_parts(main):
     header = unicode(render.header())
@@ -34,20 +58,29 @@ def return_composite_parts(main):
 
 class search:
     def GET(self):
+        self.lyric = ""
         data = web.input()
         query = data.get("q")
         print "Finding track %s" % query
-        results = pyella.search_tracks(query, "fulltracks")
-        track = results.get_next_page()[0]
-        print track.get_mbid(), track.get_title(), track.get_artist_name()
-        print track.get_attribute("rhythm_bpm_value")
-        #mbid = track.get_attribute("musicbrainz_track_url")#[29:-5]
-        links = track.get_links()
-        mblink = links[3][1]
-        mbid = mblink[29:-5]
-        self.lyric = get_musixmatch_lyric("http://api.musixmatch.com/ws/1.1/track.lyrics.get?track_mbid=%s&apikey=905e7eec68193e3f0a81a75591b4a518&format=json" % mbid, None)
-        self.lyric = re.sub(r"\r\n", "<br/>", self.lyric)
+        results = pyella.search_tracks(query, "fulltracks").get_next_page()
+        if results:
+            track = results[0]
+            print track.get_mbid(), track.get_title(), track.get_artist_name()
+            print track.get_attribute("rhythm_bpm_value")
+            links = track.get_links()
+            mblink = links[3][1]
+            if mblink:
+                #Here we have the MusicBrainz ID
+                mbid = get_mbi_from_url(mblink)
+                self.lyric = get_musixmatch_lyric(mbid)
+                print self.lyric
+            else:
+                print "No MusicBrainz ID found... trying with musixmatch"
+                self.lyric = get_musixmatch_lyric_by_query(track.get_title())
+        else:
+            self.lyric = "Not found the lyric :("
         print self.lyric
+        self.lyric = re.sub(r"\n", "<br/>", self.lyric)
         main = unicode(render.main(self))
         return return_composite_parts(main)
 
